9f86e5e93e03870bde29a1dfcb5857253f622e7f
Ebrahim Mahi <87830401+ebrahimmahi@users.noreply.github.com>
2026-04-28 21:48:35 +0600

    rgb random color generation added

--- main.js
+++ main.js
@@ -1,25 +1,62 @@
 
-const container = document.querySelector(".container");
+const body = document.querySelector("body")
+body.style.display = "flex"
+body.style.flexDirection = "column"
+body.style.justifyContent = "center"
+body.style.alignItems = "center"
+body.style.textAlign = "center"
 
-const square = document.createElement("div");
+const input = document.querySelector(".input")
 
-const body = document.querySelector("body");
-body.style.display = "flex";
-body.style.alignItems = "center";
-body.style.justifyContent = "center";
+const header = document.createElement("h3")
+header.textContent = "Enter Number of Squares Per Side: (less than 100)"
+input.appendChild(header)
 
-square.style.height = "150px";
-square.style.width = "150px";
+const inputBox = document.createElement("input")
+inputBox.id = "box-number"
+input.appendChild(inputBox)
+
+const btn = document.createElement("button")
+btn.textContent = "Enter"
+btn.style.marginBottom = "10px"
+input.appendChild(btn)
+
+const container = document.querySelector(".container")
+container.style.width = "500px"
+container.style.height = "500px"
+container.style.border = "1px solid black"
+container.style.display = "flex"
+container.style.flexWrap = "wrap"
+
+let num;
+btn.addEventListener("click", () => {
+    container.innerHTML = ""
+    num = Number(inputBox.value)
+    if(!num || num <= 0 || num > 100){
+        header.textContent = "Please enter a value less than or equal to 100!"
+        return
+    }
+    inputBox.value = ""
+    let size = 500 / num
+    
+    const box = document.createElement("div")
+    box.classList.add("box")
+    box.style.height = `${size}px`
+    box.style.width = `${size}px`
+    
+    for(let i = 0; i < num * num; i++){
+        const boxes = box.cloneNode(true)
+        container.appendChild(boxes)
+        boxes.addEventListener("mouseover", () => {
+            let r = Math.floor(Math.random() * 256)
+            let g = Math.floor(Math.random() * 256)
+            let b = Math.floor(Math.random() * 256)
+            boxes.style.background = `rgba(${r}, ${g}, ${b})`
+        }) 
+    }
+})
 
 
-for(let i = 0; i < 16; i++){
-    const clone = square.cloneNode(true);
-    clone.addEventListener("mouseleave", ()=> clone.style.background = "blue");
-    container.appendChild(clone)
-}
 
 
-container.style.display = "flex";
-container.style.flexWrap = "wrap";
-container.style.width = "600px";
-container.style.border = "1px solid black";
+
